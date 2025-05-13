afb7b948903ea00d2853022fa097252cef7ee65f
lumibee <1668651589@qq.com>
2025-05-13 16:15:03 +0800

    第一次提交测试

---
+++ src/test/java/com/lumibee/hive/HiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lumibee.hive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
