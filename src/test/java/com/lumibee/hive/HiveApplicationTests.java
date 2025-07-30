f999777f3b8de499f449b36fe70d0cd4a2d5e68e
lumibee <1668651589@qq.com>
2025-07-30 21:05:15 +0800

    修改了一些bug

--- src/test/java/com/lumibee/hive/HiveApplicationTests.java
+++ src/test/java/com/lumibee/hive/HiveApplicationTests.java
@@ -3,10 +3,12 @@ package com.lumibee.hive;
 import com.lumibee.hive.service.ArticleRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class HiveApplicationTests {
 
     @MockitoBean
