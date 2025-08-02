c8bc374cd4b8745f06d09e9aeeae097738ed1e39
lumibee <1668651589@qq.com>
2025-08-02 11:34:42 +0800

    修改了一些bug

--- src/main/java/com/lumibee/hive/config/FileUploadConfig.java
+++ src/main/java/com/lumibee/hive/config/FileUploadConfig.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class FileUploadConfig implements WebMvcConfigurer {
     
-    @Value("${file.upload.dir:/data/uploads}")
+    @Value("${file.upload.dir:./uploads}")
     private String uploadDir;
     
     @Value("${file.base.url:/uploads/}")
