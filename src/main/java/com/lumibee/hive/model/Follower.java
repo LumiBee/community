bbc7c7c2c3ba55a000760f83203cb660e817d141
lumibee <1668651589@qq.com>
2025-11-17 23:25:14 +0800

    添加注解，指定表名

--- src/main/java/com/lumibee/hive/model/Follower.java
+++ src/main/java/com/lumibee/hive/model/Follower.java
@@ -2,11 +2,13 @@ package com.lumibee.hive.model;
 
 import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
+@TableName("following")
 public class Follower {
     private Long userId;
     private Long followerId;
